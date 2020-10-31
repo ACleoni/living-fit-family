b34b8dd666a76c472e2880e76c63fcf12ac290e8
Cleoni, Alexander L <ALEXANDER_L_CLEONI@homedepot.com>
2020-10-31 02:03:55 -0400

    landing

--- src/components/fullpage/landing/Landing.tsx
+++ src/components/fullpage/landing/Landing.tsx
@@ -1,15 +1,37 @@
 import React from 'react';
+import styles from './Landing.module.scss';
 
 export default function Landing() {
+  const isAdminMode: boolean = false;
 
-    const isAdminMode: boolean = false;
+  return (
+    <div id={styles.landing}>
+      <div id={styles.filter}>
+        <div id={styles.heroContainer}>
+          <div id={styles.heroContent}>
+            <h1 id={styles.heroTitle}>A RANDOM TITLE</h1>
 
-    return (
-        <div style={{ width: '100vw', height: '90vh', overflow: "hidden", display: 'inline-block', textAlign: 'center', position: 'absolute', top: 0, left: 0 }}>
-            <div style={{ zIndex: 10, width: '100%', height: '90vh', backgroundColor: 'rgba(0, 0, 0, 0.5)', position: 'absolute', top: 0, left: 0 }} />
-            <video width='100%' height='100%' data-keepplaying muted autoPlay loop playsInline style={{ zIndex: 1, objectFit: 'cover' }}>
-                <source className="video-source" src="https://static.obefitness.com/media/hero_desktop.mp4" type="video/mp4" />
-            </video>
+            <p id={styles.heroText}>Some random text to describe the random title</p>
+
+            <a href='/#services'>
+              <div className={`${styles.scrollDownArrow} ${styles.scrollDownLink}`}></div>
+              {/* <button id={styles.heroButton}>Next Section</button> */}
+            </a>
+          </div>
         </div>
-    )
+      </div>
+      <video
+        width='100%'
+        height='100%'
+        data-keepplaying
+        muted
+        autoPlay
+        loop
+        playsInline
+        id={styles.video}
+      >
+        <source className='video-source' src='https://static.obefitness.com/media/hero_desktop.mp4' type='video/mp4' />
+      </video>
+    </div>
+  );
 }
