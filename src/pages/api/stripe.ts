f5df3d6d825439c4b0ec0aadc868a8fc782609f6
Cleoni, Alexander L <ALEXANDER_L_CLEONI@homedepot.com>
2020-11-03 15:56:24 -0500

    token implemented

--- src/pages/api/stripe.ts
+++ src/pages/api/stripe.ts
@@ -22,7 +22,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         res.status(200).json(session);
       }
     } catch (error) {
-      
+      console.log(error);
     }
   } else {
     res.status(405);
