8449c7a238846d273cf6f0421dee3da5110ea327
Cleoni, Alexander L <ALEXANDER_L_CLEONI@homedepot.com>
2020-11-11 20:08:17 -0500

    login

--- src/components/fullpage/services/Services.tsx
+++ src/components/fullpage/services/Services.tsx
@@ -29,12 +29,7 @@ export default function Services() {
             <a href='#'>Learn more</a>
           </div>
           <div className={styles.card}>
-            <img
-              style={{ objectFit: 'cover' }}
-              height={200}
-              width={330}
-              src='https://res.cloudinary.com/dq0fvempx/image/upload/v1604765261/group-training_pcl77h.jpg'
-            />
+            <img src='https://images.unsplash.com/photo-1518310383802-640c2de311b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=330&h=200' />
             <h2 className={styles.card_header}>Group Training</h2>
             <p className={styles.text}>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos molestias facere.
@@ -42,7 +37,11 @@ export default function Services() {
             <a href='#'>Learn more</a>
           </div>
           <div className={styles.card}>
-            <img src='https://images.unsplash.com/photo-1518310383802-640c2de311b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=330&h=200' />
+            <img
+              height='200'
+              width='330'
+              src='https://media1.popsugar-assets.com/files/thumbor/1pmhFU8rXf5FfTQA9VZkjOxEFCc/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2020/05/21/758/n/1922729/dd030108b61e428e_GettyImages-1190436759/i/I-Do-Live-Workout-Classes-on-Zoom.jpg'
+            />
             <h2 className={styles.card_header}>Zoom Sessions</h2>
             <p className={styles.text}>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos molestias facere.
