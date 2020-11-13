2fde59cf9cfd5f6b8d2752a62c52765b24d1e974
Cleoni, Alexander L <ALEXANDER_L_CLEONI@homedepot.com>
2020-11-13 00:54:41 -0500

    Mobile Header Implemented

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,33 +1,31 @@
 import React, { Component } from 'react';
-import GoogleMapReact from 'google-map-react';
 // import marker from '../../../public/maps-and-flags.png'
 
 // const AnyReactComponent = ({ lat, lng, text }) => <img height='25px' src={marker} />;
 
-
 interface MyProps {
-    center: object,
-    zoom: number
+  center: object;
+  zoom: number;
 }
 
 interface MyState {
-    value: string
+  value: string;
 }
 
 class SimpleMap extends Component<MyProps, MyState> {
-    static defaultProps = {
-        center: {
-            lat: 33.878030,
-            lng: -84.504320
-        },
-        zoom: 12
-    };
+  static defaultProps = {
+    center: {
+      lat: 33.87803,
+      lng: -84.50432,
+    },
+    zoom: 12,
+  };
 
-    render() {
-        return (
-            // Important! Always set the container height explicitly
-            <div style={{ height: '100%', width: '100%' }}>
-                {/* <GoogleMapReact
+  render() {
+    return (
+      // Important! Always set the container height explicitly
+      <div style={{ height: '100%', width: '100%' }}>
+        {/* <GoogleMapReact
                     bootstrapURLKeys={{ key: 'AIzaSyApwGRfYpmOiQqNtmq8fvHH9zyj9G_t320' }}
                     defaultCenter={this.props.center}
                     defaultZoom={this.props.zoom}
@@ -38,9 +36,9 @@ class SimpleMap extends Component<MyProps, MyState> {
                         text="My Marker"
                     />
                 </GoogleMapReact> */}
-            </div>
-        );
-    }
+      </div>
+    );
+  }
 }
 
 export default SimpleMap;
