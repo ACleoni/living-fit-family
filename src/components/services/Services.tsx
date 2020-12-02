5a630b1e30d63079f18af28cfe48807ea42172e6
Cleoni, Alexander L <ALEXANDER_L_CLEONI@homedepot.com>
2020-12-02 16:56:23 -0500

    new image

--- src/components/services/Services.tsx
+++ src/components/services/Services.tsx
@@ -4,26 +4,33 @@ import styles from './Services.module.scss';
 
 export default function Services() {
   return (
-    <Grid
-      container
-      style={{ padding: '5%', background: 'rgba(0, 0, 0, 1)', color: '#f1f1f1', justifyContent: 'center' }}
+    <div
+      style={{
+        padding: '5%',
+        marginTop: '60px',
+        background: 'rgba(0, 0, 0, 1)',
+        color: '#f1f1f1',
+        justifyContent: 'center',
+      }}
     >
-      <Grid item>
+      {/* <Grid item> */}
+      <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-around', textAlign: 'center' }}>
         <Typography variant='h5'>High Intensity Group Training</Typography>
-        <Typography variant='caption'>
+        <Typography variant='subtitle1' style={{ padding: '10% 0 10% 0', color: 'grey' }}>
           Stength and functional movements designed for lean muscle gains and fat loss. Sessions up to 1 hour long with
           thorough instruction, demonstration and guidance.
         </Typography>
-        <br />
-        <Typography variant='caption'>Sessions available weekly from 2 days to unlimited.</Typography>
-        <div style={{ width: '100%' }}>
-          <img
-            style={{ width: '100%' }}
-            src='https://res.cloudinary.com/dq0fvempx/image/upload/v1606858378/group-training_pcl77h_1_fstquu.png'
-          />
-        </div>
-      </Grid>
-    </Grid>
+        <Typography variant='subtitle1'>Sessions available weekly from 2 days to unlimited.</Typography>
+      </div>
+      <div style={{ width: '100%' }}>
+        <img
+          style={{ width: '100%' }}
+          src='https://res.cloudinary.com/dq0fvempx/image/upload/v1606946154/Screen_Shot_2020-12-02_at_4.55.39_PM_ahpbnw.png'
+        />
+      </div>
+    </div>
+    // {/* </Grid> */}
+    // {/* </Grid> */}
     //     {/* <div className='uk-width-small-* uk-text-center'>
     //       <h1 className='uk-text-light'>High intensity group training</h1>
     //       <p className='uk-text-light uk-text-emphasis'>
