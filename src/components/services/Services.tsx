7ef188af4449b8ad36a5bed661ed19e1be179fcc
Cleoni, Alexander L <ALEXANDER_L_CLEONI@homedepot.com>
2020-12-27 16:36:47 -0500

    fix services

--- src/components/services/Services.tsx
+++ src/components/services/Services.tsx
@@ -71,7 +71,37 @@ export default function Services() {
 
       <Divider style={{ height: '12px', background: '#fff' }} />
 
-      <GroupTraining />
+      <Grid container item style={{ padding: '10%', backgroundColor: '#fbfbfd' }}>
+        <Grid container alignItems='center' justify='flex-end' spacing={1}>
+          <Grid container item xs={12} direction='column' justify='center' alignItems='center'>
+            <LiveTvIcon fontSize='large' style={{ color: 'black' }} />
+            <Typography align='center' variant='h4' color='textPrimary' style={{ fontWeight: 600 }}>
+              Group Sessions
+            </Typography>
+          </Grid>
+
+          <Grid item xs={12}>
+            <Typography align='center' variant='body1' color='textPrimary'>
+              High intensity, functional, strength-based training. Designed for fat loss and lean muscle gains.
+            </Typography>
+          </Grid>
+
+          <Grid container item xs={12} justify='center'>
+            <Button className={classes.button} size='small' variant='text' color='secondary'>
+              Connect
+            </Button>
+            <Button className={classes.button} size='small' variant='outlined' color='secondary'>
+              Learn More
+            </Button>
+          </Grid>
+          <Grid container item xs={12} justify='center'>
+            <img
+              style={{ width: '100%', alignSelf: 'center', marginTop: '25px' }}
+              src='https://res.cloudinary.com/dq0fvempx/image/upload/v1608245613/oie_transparent_3_fpimwo.png'
+            />
+          </Grid>
+        </Grid>
+      </Grid>
 
       <Divider style={{ height: '12px', background: '#fff' }} />
 
